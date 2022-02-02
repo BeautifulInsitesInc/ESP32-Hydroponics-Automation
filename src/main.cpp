b879eabf2423f07d92c1f3b3e47537b99ea60c66
Savvy Insights <greg@savvyinsights.io>
2022-02-02 18:01:33 -0500

    Added calibration function to increase voltage reading accuracty on ADC pins

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
 #include <SimpleTimer.h>
+#include <esp_adc_cal.h>
+//#include <DFRobot_ESP_PH.h> // library to calcuate ph from voltage
+
+//DFRobot_ESP_PH ph;
 
 SimpleTimer timer;
 
@@ -23,13 +27,52 @@ const int ph_pin = 35; // pin the Ph sensor is connected to
 const int pump_pin = 36; // set pump pin
 const int heater_pin = 39;
 
+// *************** CALIBRATION FUNCTION ******************
+// To calibrate actual votage read at pin to the esp32 reading
+
+uint32_t readADC_Cal(int ADC_Raw)
+{
+  esp_adc_cal_characteristics_t adc_chars;
+  
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
+}
+
+// =======================================================
+// ======= TEMPURATURE SENSOR DS18B20 ====================
+// =======================================================
+#define TEMPERATURE_PRECISION 10
+OneWire oneWire(temp_pin);// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature waterTempSensor(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+
+float tempC; // tempurature in Celsius
+float tempF; // tempurature in Fahrenheit
+
+void getWaterTemp()
+  {
+    waterTempSensor.requestTemperatures();    // send the command to get temperatures
+    tempC = waterTempSensor.getTempCByIndex(0);  // read temperature in °C
+    tempF = tempC * 9 / 5 + 32; // convert °C to °F
+    //Serial.print("tempC : ");
+    //Serial.print(tempC);
+    //Serial.print("  tempF : ");
+    //Serial.println(tempF);
+    
+    if (tempC == DEVICE_DISCONNECTED_C) // Something is wrong, so return an error
+      {
+        Serial.println("Houston, we have a problem");
+        tempC = -100; 
+        tempF = tempC;
+      }
+    //delay(1000);
+  }
 // =======================================================
 // ======= PH SENSOR =====================================
 // =======================================================
 float ph_value; // actual pH value to display on screen
 //-----------------------------------------
 float calibration_adjustment_ph = 0; // adjust this to calibrate
-float voltage_adjustment = 0;
+//float voltage_adjustment = 0;
 int voltage_input_ph = 3.3; // voltage can be 5 or 3.3
 //-----------------------------------------
 float adc_resolution = 4095;
@@ -65,14 +108,23 @@ void getPH()
       {
         average_value_ph += buffer_array_ph[i];
       }
-    float voltage_ph = (float)average_value_ph * voltage_input_ph / adc_resolution / 6; 
+    average_value_ph = average_value_ph / 6;
+
+    //float voltage_ph = (float)average_value_ph * voltage_input_ph / adc_resolution / 6; 
     //voltage_ph = voltage_ph * (1+(voltage_adjustment/voltage_ph)); //  This line is to try to bring voltage to actual
-    ph_value = (-5.70 * voltage_ph) + calibration_value_ph; // Calculate the actual pH
+    
 
     //float voltage_ph = analogRead(ph_pin) * voltage_input_ph / adc_resolution;
-
-    //ph_value = voltage_input_ph * voltage_ph;
     
+    float voltage_ph = average_value_ph * voltage_input_ph / adc_resolution;
+    float calibrated_voltage = readADC_Cal(average_value_ph);
+    calibrated_voltage = calibrated_voltage/1000;
+
+    ph_value = voltage_input_ph * calibrated_voltage;
+    //ph_value = (-5.70 * calibrated_voltage) + calibration_value_ph; // Calculate the actual pH
+    //int temp_plug = 25;
+    //float calibrated_ph_value = ph.readPH(calibrated_voltage, temp_plug);
+/*
     Serial.print("analogRead(ph_pin : ");
     Serial.print(analogRead(ph_pin));
     Serial.print("    average_value_ph = ");
@@ -82,46 +134,23 @@ void getPH()
     Serial.print("     calibration_value_ph = ");
     Serial.print(calibration_value_ph);
     Serial.print("     ph_value = ");
-    Serial.println(ph_value);
+    Serial.print(ph_value);
+    Serial.print("     calibtraded_voltage = ");
+    Serial.print(calibrated_voltage);
+    Serial.print("     calibtraded_ph_value = ");
+    Serial.println(calibrated_ph_value);
 
     delay(0); // pause between serial monitor output - can be set to zero after testing
- 
+ */
   }
 
-// =======================================================
-// ======= TEMPURATURE SENSOR DS18B20 ====================
-// =======================================================
-#define TEMPERATURE_PRECISION 10
-OneWire oneWire(temp_pin);// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-DallasTemperature waterTempSensor(&oneWire); // Pass our oneWire reference to Dallas Temperature.
 
-float tempC; // tempurature in Celsius
-float tempF; // tempurature in Fahrenheit
-
-void getWaterTemp()
-  {
-    waterTempSensor.requestTemperatures();    // send the command to get temperatures
-    tempC = waterTempSensor.getTempCByIndex(0);  // read temperature in °C
-    tempF = tempC * 9 / 5 + 32; // convert °C to °F
-    //Serial.print("tempC : ");
-    //Serial.print(tempC);
-    //Serial.print("  tempF : ");
-    //Serial.println(tempF);
-    
-    if (tempC == DEVICE_DISCONNECTED_C) // Something is wrong, so return an error
-      {
-        Serial.println("Houston, we have a problem");
-        tempC = -100; 
-        tempF = tempC;
-      }
-    //delay(1000);
-  }
 
 // =======================================================
 // ======= PPM OCEAN TDS METER SENSOR ====================
 // =======================================================
 
-#define VREF 5.0      // analog reference voltage(Volt) of the ADC
+#define VREF 3.3      // analog reference voltage(Volt) of the ADC
 #define SCOUNT  30           // sum of sample point
 int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
@@ -178,10 +207,14 @@ void getTDSReading()
           analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
           float medianRead = getMedianNum(analogBuffer,SCOUNT);
           averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0; // ESP32 - 1-4096 - Ardurino mega 0-1023read the analog value more stable by the median filtering algorithm, and convert to voltage value
+          
+          //float calibrated_voltage = readADC_Cal(medianRead);
+          //calibrated_voltage = calibrated_voltage/1000;
+          
           float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
           float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
           tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
-          /*
+          
           Serial.print("   VREF : ");
           Serial.print(VREF);
           Serial.print("   analogRead(tds_pin) : ");
@@ -190,11 +223,15 @@ void getTDSReading()
           Serial.print(medianRead);
           Serial.print("   averageVoltage: ");
           Serial.print(averageVoltage,2);
+          //Serial.print("   calibrated_voltage: ");
+          //Serial.print(calibrated_voltage,2);
           Serial.print("   TtdsValue: ");
           Serial.print(tdsValue, 0);
           Serial.print("   Tempurature: ");
-          Serial.println(temperature);
-        */
+          Serial.print(temperature);
+          Serial.print("   compensationVoltage: ");
+          Serial.println(compensationVolatge);
+        
       }
     delay(0);
   }
@@ -297,6 +334,7 @@ void setup(void)
     // Initialize Sensors
     waterTempSensor.begin(); // initalize water temp sensor
     pinMode(tds_pin,INPUT); // setup TDS sensor pin
+    //ph.begin(); // starts the ph api 
 
     // Set relay pins
     pinMode(pump_pin, OUTPUT);
@@ -325,4 +363,5 @@ void loop(void)
 // ----------------- END MAIN LOOP ------------------------
 
 
+ 
 
